56a5ea4038beaa9bc5af656a291107daeed184f2
Andy <Andy@2017Build>
2022-08-08 21:12:29 -0400

    initial project commit

---
+++ src/main/java/org/fhbc/botn/BeerOfTheNightApplication.java
@@ -0,0 +1,13 @@
+package org.fhbc.botn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerOfTheNightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerOfTheNightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fhbc/botn/BeerOfTheNightApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fhbc.botn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerOfTheNightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
