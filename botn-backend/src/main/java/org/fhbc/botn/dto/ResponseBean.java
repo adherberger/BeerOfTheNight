aa3e213ec7e3ddbe17d50f1e100eea24a6276ec8
Zachary Herberger <zherberger@gmail.com>
2022-10-14 19:00:17 -0400

    Add UI project, and put backend project in its own folder

---
+++ botn-backend/src/main/java/org/fhbc/botn/dto/ResponseBean.java
@@ -0,0 +1,16 @@
+package org.fhbc.botn.dto;
+
+public class ResponseBean {
+
+	private int GameId;
+
+	public int getGameId() {
+		return GameId;
+	}
+
+	public void setGameId(int gameId) {
+		GameId = gameId;
+	}
+	
+	
+}
---
+++ botn-backend/src/main/java/org/fhbc/botn/entity/GameEntity.java
@@ -0,0 +1,54 @@
+package org.fhbc.botn.entity;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "game_main")
+// Top level table for keeping track of games
+public class GameEntity {
+	
+	@Id
+	@GeneratedValue(generator="game_id_seq")
+	//@SequenceGenerator(name="game_id_seq",sequenceName="GAME_ID_SEQ", allocationSize=1)
+
+	@Column(name = "game_id")
+	private int gameId;
+	
+	@Column(name = "game_state")
+	private String gameState;
+	
+	@Column(name = "game_date")
+	private Timestamp gameDate;
+	
+	public int getGameId() {
+		return gameId;
+	}
+
+	public void setGameId(int gameId) {
+		this.gameId = gameId;
+	}
+
+	public String getGameState() {
+		return gameState;
+	}
+
+	public void setGameState(String gameState) {
+		this.gameState = gameState;
+	}
+
+	public Timestamp getGameDate() {
+		return gameDate;
+	}
+
+	public void setGameDate(Timestamp gameDate) {
+		this.gameDate = gameDate;
+	}
+
+}
