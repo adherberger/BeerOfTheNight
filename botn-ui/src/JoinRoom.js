85cf9fa6701afc3efbb20784937b7d42eb577cd1
Zachary Herberger <zherberger@gmail.com>
2022-10-28 23:49:02 -0400

    Messing around more with the Join Room page

--- botn-ui/src/JoinRoom.js
+++ botn-ui/src/JoinRoom.js
@@ -8,42 +8,57 @@ const StateInput = ({
     id,
     title,
     stateVar,
-    setStateVar
+    setStateVar,
+    ...props
 }) => {
     return (
-        <>
+        <div className="botn-input">
             {
                 title ?
-                <label for={id}>{title}</label>
-                : <></>
+                    <label for={id} className="text-input-label">{title}</label>
+                    : <></>
             }
-            <input id={id} value={stateVar} onChange={(e) => {setStateVar(e.target.value)}}/>
-        </>
+            <input
+                id={id}
+                className="text-input"
+                value={stateVar}
+                onChange={(e) => { setStateVar(e.target.value) }}
+                {...props}
+            />
+        </div>
     );
 }
 
 const JoinRoom = () => {
     const gameContext = useGameContext();
-    const [ name, setName ] = useState("");
-    const [ roomCode, setRoomCode ]  = useState("");
+    const [name, setName] = useState("");
+    const [roomCode, setRoomCode] = useState("");
+    const [roomCodeNotFound, setRoomCodeNotFound] = useState(false);
     const navigate = useNavigate();
+
+    const updateRoomCode = (val) => {
+        val = val.toUpperCase();
+        setRoomCode(val);
+    }
 
     const joinGame = () => {
         axios.post(
             BOTN_JOIN_GAME,
             { name: name, roomCode: roomCode }
         ).then((response) => {
-            if(response.status === 200) {
+            if (response.status === 200) {
                 gameContext.setGame(response.data);
                 navigate.push("/lobby");
+            } else if (response.status === 404) {
+                setRoomCodeNotFound(true);
             }
         })
     }
 
     return (
         <div className="main-page">
-            <StateInput id="name-input" title="Your Name" stateVar={name} setStateVar={setName}/>
-            <StateInput id="room-code-input" title="Room Code" stateVar={roomCode} setStateVar={setRoomCode}/>
+            <StateInput id="name-input" title="Your Name" stateVar={name} setStateVar={setName} />
+            <StateInput id="room-code-input" title="Room Code" stateVar={roomCode} setStateVar={updateRoomCode} maxLength={4} />
             <button className="button-main" onClick={() => { joinGame(); }}>Join Room</button>
         </div>
     );
