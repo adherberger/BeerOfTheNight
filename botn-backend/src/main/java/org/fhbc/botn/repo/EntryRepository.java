cb70da03808674adb0bee8fefd9b050d75b93f6a
Zachary Herberger <zherberger@gmail.com>
2022-12-03 10:05:01 -0500

    Re-added a query to EntryRepository, haven't had a chance to test but it will be a while til we need to get all the entries for a given game.

--- botn-backend/src/main/java/org/fhbc/botn/repo/EntryRepository.java
+++ botn-backend/src/main/java/org/fhbc/botn/repo/EntryRepository.java
@@ -1,7 +1,10 @@
 package org.fhbc.botn.repo;
 
+import java.util.List;
+
 import org.fhbc.botn.entity.EntryEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface EntryRepository extends CrudRepository<EntryEntity, Integer> {
+	List<EntryEntity> findAllByGame_GameId(int gameId);
 }
